b0fda3987802a912c7fd1d4718211c54441f6cd6
RodrigoAHQ <rodrigo@gmail.com>
2025-10-26 21:43:39 -0500

    Entidad Apoderado

--- CapaEntidad/entApoderado.cs
+++ CapaEntidad/entApoderado.cs
@@ -8,5 +8,12 @@ namespace CapaEntidad
 {
     internal class entTutor
     {
+        public int IdApoderado { get; set; }
+        public string DniApo { get; set; }
+        public string NombreApo { get; set; }
+        public string ApellidosApo { get; set; }
+        public string NumCelularApo { get; set; }
+        public string DireccionApo { get; set; }
+        public bool EstadoApot { get; set; }
     }
 }

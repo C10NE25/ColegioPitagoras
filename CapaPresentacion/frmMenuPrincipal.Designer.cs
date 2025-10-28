5643b299dcd3b04d7707b529e5aa98dbeef2086d
Francesco Pastor Bustamante <cesco.pab@gmail.com>
2025-10-28 00:43:23 -0500

    Cambio de nombre de Form1 a frmMenuPrincipal

--- CapaPresentacion/frmMenuPrincipal.Designer.cs
+++ CapaPresentacion/frmMenuPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CapaPresentacion
 {
-    partial class Form1
+    partial class frmMenuPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuPrincipal));
             this.panelMenuLateral = new System.Windows.Forms.Panel();
             this.panelAcademicoSubMenu = new System.Windows.Forms.Panel();
             this.btnGradoAcademico = new System.Windows.Forms.Button();

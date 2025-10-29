df17b72ced353e14ab7f6a3aaac9edff42bbb61c
Francesco Pastor Bustamante <cesco.pab@gmail.com>
2025-10-28 22:17:55 -0500

    Funcionalidad de frmTutor

--- CapaPresentacion/frmAcademico/frmTutor.Designer.cs
+++ CapaPresentacion/frmAcademico/frmTutor.Designer.cs
@@ -28,41 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvTutor = new System.Windows.Forms.DataGridView();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnCerrar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.gbxTutor = new System.Windows.Forms.GroupBox();
-            this.txtDniTutor = new System.Windows.Forms.TextBox();
-            this.txtIDTutor = new System.Windows.Forms.TextBox();
-            this.txtNombreTutor = new System.Windows.Forms.TextBox();
-            this.txtApellidoTutor = new System.Windows.Forms.TextBox();
-            this.txtNroCelularTutor = new System.Windows.Forms.TextBox();
-            this.txtDireccionTutor = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.cbxEstado = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDireccionTutor = new System.Windows.Forms.TextBox();
+            this.txtNroCelularTutor = new System.Windows.Forms.TextBox();
+            this.txtApellidoTutor = new System.Windows.Forms.TextBox();
+            this.txtNombreTutor = new System.Windows.Forms.TextBox();
+            this.txtIDTutor = new System.Windows.Forms.TextBox();
+            this.txtDniTutor = new System.Windows.Forms.TextBox();
             this.btnNuevo = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnDeshabilitar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTutor)).BeginInit();
             this.gbxTutor.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvTutor
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(127, 173);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 92;
-            this.dataGridView1.RowTemplate.Height = 37;
-            this.dataGridView1.Size = new System.Drawing.Size(1432, 601);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvTutor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTutor.Location = new System.Drawing.Point(127, 173);
+            this.dgvTutor.Name = "dgvTutor";
+            this.dgvTutor.ReadOnly = true;
+            this.dgvTutor.RowHeadersWidth = 92;
+            this.dgvTutor.RowTemplate.Height = 37;
+            this.dgvTutor.Size = new System.Drawing.Size(1432, 601);
+            this.dgvTutor.TabIndex = 0;
+            this.dgvTutor.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTutor_CellDoubleClick);
             // 
             // btnAgregar
             // 
@@ -73,6 +76,7 @@
             this.btnAgregar.TabIndex = 1;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnModificar
             // 
@@ -83,6 +87,7 @@
             this.btnModificar.TabIndex = 2;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
             // 
             // btnCancelar
             // 
@@ -93,10 +98,11 @@
             this.btnCancelar.TabIndex = 3;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(1664, 494);
+            this.btnCerrar.Location = new System.Drawing.Point(1664, 640);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(230, 95);
             this.btnCerrar.TabIndex = 4;
@@ -139,102 +145,6 @@
             this.gbxTutor.TabStop = false;
             this.gbxTutor.Text = "Datos del Tutor";
             // 
-            // txtDniTutor
-            // 
-            this.txtDniTutor.Location = new System.Drawing.Point(179, 57);
-            this.txtDniTutor.Name = "txtDniTutor";
-            this.txtDniTutor.Size = new System.Drawing.Size(155, 35);
-            this.txtDniTutor.TabIndex = 0;
-            // 
-            // txtIDTutor
-            // 
-            this.txtIDTutor.Location = new System.Drawing.Point(179, 124);
-            this.txtIDTutor.Name = "txtIDTutor";
-            this.txtIDTutor.Size = new System.Drawing.Size(338, 35);
-            this.txtIDTutor.TabIndex = 1;
-            // 
-            // txtNombreTutor
-            // 
-            this.txtNombreTutor.Location = new System.Drawing.Point(179, 186);
-            this.txtNombreTutor.Name = "txtNombreTutor";
-            this.txtNombreTutor.Size = new System.Drawing.Size(338, 35);
-            this.txtNombreTutor.TabIndex = 2;
-            // 
-            // txtApellidoTutor
-            // 
-            this.txtApellidoTutor.Location = new System.Drawing.Point(179, 251);
-            this.txtApellidoTutor.Name = "txtApellidoTutor";
-            this.txtApellidoTutor.Size = new System.Drawing.Size(338, 35);
-            this.txtApellidoTutor.TabIndex = 3;
-            // 
-            // txtNroCelularTutor
-            // 
-            this.txtNroCelularTutor.Location = new System.Drawing.Point(179, 337);
-            this.txtNroCelularTutor.Name = "txtNroCelularTutor";
-            this.txtNroCelularTutor.Size = new System.Drawing.Size(338, 35);
-            this.txtNroCelularTutor.TabIndex = 4;
-            // 
-            // txtDireccionTutor
-            // 
-            this.txtDireccionTutor.Location = new System.Drawing.Point(827, 107);
-            this.txtDireccionTutor.Name = "txtDireccionTutor";
-            this.txtDireccionTutor.Size = new System.Drawing.Size(338, 35);
-            this.txtDireccionTutor.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 57);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 29);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Id del Tutor:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 130);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 29);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "DNI:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 195);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(119, 29);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Nombres:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(27, 257);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(120, 29);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Apellidos:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(27, 343);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(142, 29);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Nro Celular:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(651, 113);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(121, 29);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Direccion:";
-            // 
             // cbxEstado
             // 
             this.cbxEstado.AutoSize = true;
@@ -245,6 +155,102 @@
             this.cbxEstado.Text = "Estado del Tutor";
             this.cbxEstado.UseVisualStyleBackColor = true;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(651, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 29);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Direccion:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 343);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Nro Celular:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Apellidos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nombres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "DNI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Id del Tutor:";
+            // 
+            // txtDireccionTutor
+            // 
+            this.txtDireccionTutor.Location = new System.Drawing.Point(827, 107);
+            this.txtDireccionTutor.Name = "txtDireccionTutor";
+            this.txtDireccionTutor.Size = new System.Drawing.Size(338, 35);
+            this.txtDireccionTutor.TabIndex = 5;
+            // 
+            // txtNroCelularTutor
+            // 
+            this.txtNroCelularTutor.Location = new System.Drawing.Point(179, 337);
+            this.txtNroCelularTutor.Name = "txtNroCelularTutor";
+            this.txtNroCelularTutor.Size = new System.Drawing.Size(338, 35);
+            this.txtNroCelularTutor.TabIndex = 4;
+            // 
+            // txtApellidoTutor
+            // 
+            this.txtApellidoTutor.Location = new System.Drawing.Point(179, 251);
+            this.txtApellidoTutor.Name = "txtApellidoTutor";
+            this.txtApellidoTutor.Size = new System.Drawing.Size(338, 35);
+            this.txtApellidoTutor.TabIndex = 3;
+            // 
+            // txtNombreTutor
+            // 
+            this.txtNombreTutor.Location = new System.Drawing.Point(179, 186);
+            this.txtNombreTutor.Name = "txtNombreTutor";
+            this.txtNombreTutor.Size = new System.Drawing.Size(338, 35);
+            this.txtNombreTutor.TabIndex = 2;
+            // 
+            // txtIDTutor
+            // 
+            this.txtIDTutor.Location = new System.Drawing.Point(179, 54);
+            this.txtIDTutor.Name = "txtIDTutor";
+            this.txtIDTutor.Size = new System.Drawing.Size(139, 35);
+            this.txtIDTutor.TabIndex = 1;
+            // 
+            // txtDniTutor
+            // 
+            this.txtDniTutor.Location = new System.Drawing.Point(179, 127);
+            this.txtDniTutor.Name = "txtDniTutor";
+            this.txtDniTutor.Size = new System.Drawing.Size(338, 35);
+            this.txtDniTutor.TabIndex = 0;
+            // 
             // btnNuevo
             // 
             this.btnNuevo.Location = new System.Drawing.Point(1664, 207);
@@ -253,6 +259,7 @@
             this.btnNuevo.TabIndex = 7;
             this.btnNuevo.Text = "Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
             // btnEditar
             // 
@@ -262,21 +269,33 @@
             this.btnEditar.TabIndex = 8;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnDeshabilitar
+            // 
+            this.btnDeshabilitar.Location = new System.Drawing.Point(1664, 497);
+            this.btnDeshabilitar.Name = "btnDeshabilitar";
+            this.btnDeshabilitar.Size = new System.Drawing.Size(230, 95);
+            this.btnDeshabilitar.TabIndex = 9;
+            this.btnDeshabilitar.Text = "Deshabilitar";
+            this.btnDeshabilitar.UseVisualStyleBackColor = true;
+            this.btnDeshabilitar.Click += new System.EventHandler(this.btnDeshabilitar_Click);
             // 
             // frmTutor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1987, 1374);
+            this.Controls.Add(this.btnDeshabilitar);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnNuevo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCerrar);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvTutor);
             this.Controls.Add(this.gbxTutor);
             this.Name = "frmTutor";
             this.Text = "frmTutor";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTutor)).EndInit();
             this.gbxTutor.ResumeLayout(false);
             this.gbxTutor.PerformLayout();
             this.ResumeLayout(false);
@@ -286,7 +305,7 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvTutor;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnCancelar;
@@ -308,5 +327,6 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btnNuevo;
         private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnDeshabilitar;
     }
 }

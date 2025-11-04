add26c5644e029f4dac4151b50c6a0cf5a40bada
RodrigoAHQ <rodrigo@gmail.com>
2025-11-04 08:06:47 -0500

    Entidad parentesco

---
+++ CapaEntidad/Parentesco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Parentesco
+    {
+        public int idParentesco { get; set; }
+
+        public string tipoParentesco { get; set; }
+    }
+}

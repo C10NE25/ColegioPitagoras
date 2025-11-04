04a848aefb93a40763d8b8fad23bb628df162d40
RodrigoAHQ <rodrigo@gmail.com>
2025-11-04 08:41:21 -0500

    Entidades para los CORES

---
+++ CapaEntidad/entAsistencia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entAsistencia
+    {
+        public int IdAsistencia { get; set; }
+        public int IdEstudiante { get; set; }
+        public DateTime Fecha { get; set; }
+        public int IdTipoAsistencia { get; set; }
+    }
+}
---
+++ CapaEntidad/entInscripcion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entInscripcion
+    {
+        public int IdInscripcion { get; set; }
+        public int IdEstudiante { get; set; }
+        public DateTime FechaInscripcion { get; set; }
+        public bool EstadoInscripcion { get; set; }
+    }
+}

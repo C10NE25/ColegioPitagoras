7605956637b9bc7b90e1c7bd7e3ba4d4f19ddd9f
RodrigoAHQ <rodrigo@gmail.com>
2025-11-06 19:55:38 -0500

    logica y datos

---
+++ CapaLogica/logPeriodoAcademico.cs
@@ -0,0 +1,48 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class logPeriodoAcademico
+    {
+        #region singleton
+
+        private static readonly logPeriodoAcademico _instancia = new logPeriodoAcademico();
+
+        public static logPeriodoAcademico Instancia
+        {
+            get
+            {
+                return logPeriodoAcademico._instancia;
+            }
+        }
+        #endregion singleton
+
+        #region metodos
+        public List<entPeriodoAcademico> ListarPeriodoAcademico()
+        {
+            return datPeriodoAcademico.Instancia.listarPeriodoAcademico();
+        }
+
+        public void insertarPeriodoAcademico(entPeriodoAcademico Pa)
+        {
+            datPeriodoAcademico.Instancia.InsertarPeriodoAcademico(Pa);
+        }
+
+        public void editarPeriodoAcademico(entPeriodoAcademico Pa)
+        {
+            datPeriodoAcademico.Instancia.EditarPeriodoAcademico(Pa);
+        }
+
+        public void DeshabilitarPeriodoAcademico(entPeriodoAcademico Pa)
+        {
+            datPeriodoAcademico.Instancia.DeshabilitarPeriodoAcademico(Pa);
+        }
+        #endregion metodos
+    }
+}

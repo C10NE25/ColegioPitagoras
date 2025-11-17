c8d86b293b3082266d766b27bfa0b7e1e7c5751c
RodrigoAHQ <rodrigo@gmail.com>
2025-11-16 22:58:40 -0500

    hola

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -21,7 +21,7 @@ namespace CapaDatos
             /*Data Source= Nombre de su servidor, 
             * Initial Catalog= Nombre de la base de datos*/
             cn.ConnectionString =
-                "Data Source=C10N3\\USERDP; " +
+                "Data Source=DESKTOP-OAR169C\\SQLEXPRESS; " +
                 "Initial Catalog = BDColegioPitagoras;" +
                 "Integrated Security=true";
             return cn;
